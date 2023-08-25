1589ca64c2d59b633cc47f62f82c37a85fd6ffa5
jolanjose <jolanjose2003@gmail.com>
2023-08-25 15:09:18 +0530

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -4,8 +4,8 @@ const connectDB=async()=>{
     try{
         mongoose.set("strictQuery", false);
 
-        mongoose.connect("mongodb://127.0.0.1:27017/BARATH");
-      //  mongoose.connect("mongodb+srv://barath:987654321@cluster0.ooxe0gr.mongodb.net/?retryWrites=true&w=majority");
+        // mongoose.connect("mongodb://127.0.0.1:27017/BARATH");
+        mongoose.connect("mongodb+srv://barath:987654321@cluster0.ooxe0gr.mongodb.net/?retryWrites=true&w=majority");
 
   console.log("Connected to MongoDB");
        
